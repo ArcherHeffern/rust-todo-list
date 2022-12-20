26cab9802b2ee65ba776ffa09001aea6a2e942ef
Archer <heffernarcher@gmail.com>
2022-12-19 21:04:12 -0500

    added CRUD routes

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,7 +5,22 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[put("/")]
+fn put() -> &'static str {
+    "Updated"
+}
+
+#[post("/<id>")]
+fn post(id: &str) -> String {
+    format!("{}", id)
+}
+
+#[delete("/<id>")]
+fn delete(id: &str) -> String {
+    format!("{id}")
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/api/", routes![index, put, post, delete])
 }
